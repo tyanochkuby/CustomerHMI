6c0ebd4979c8e17f15e43213a08d91075bb577fd
Raman Yudzeshka <raman.yudzeshka@macrix.pl>
2024-09-10 15:20:22 +0200

    Fix postal code completion

---
+++ CustomersTableUnitTests/IndexPageTests.cs
@@ -0,0 +1,34 @@
+﻿using CustomersTable.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersTableUnitTests
+{
+    public class IndexPageTests
+    {
+        [Fact]
+        public void CancelChanges_ShouldRevertCustomerListToOriginal()
+        {
+            // Arrange
+            var page = new Index();
+            var originalList = new List<Customer>
+            {
+                new Customer {  
+                                FirstName = "Jan", 
+                                LastName = "Kowalski",
+                                StreetName = "Kwiatowa",
+                                HouseNumber = "1",
+                                PostalCode = "00-001",
+                                Town = "Warszawa",
+                                PhoneNumber = "123456789",
+                                BirthDate = new DateTime(1990, 1, 1),
+                                Age = 31
+                }
+            };
+            
+        }
+    }
+}
